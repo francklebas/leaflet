cf5529d38db6475617a5674de13e60364dd11145
Franck LEBAS <francklebas@ik.me>
2026-04-28 11:20:57 +0200

    feat(init): load a map

---
+++ src/components/LeafletMap.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import L from "leaflet";
+import { onMounted } from "vue";
+
+onMounted(() => {
+  L.map("map").setView([51.505, -0.09], 13);
+});
+</script>
+
+<template>
+  <div id="map">map</div>
+</template>
+
+<style lang="scss" scoped>
+#map {
+  height: 180px;
+}
+</style>
